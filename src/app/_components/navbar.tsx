c71f38b503a5f14323ffcd5e0d0e908cca99bfd0
Danish <dan71ish@gmail.com>
2024-08-30 09:59:23 +0530

    navbar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,5 +1,13 @@
+import { Button } from "~/components/ui/button";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between px-4 py-2 md:px-8 md:py-4">
+      <h2>Job Portal</h2>
+
+      <Button variant={"outline"}>Get Started</Button>
+    </nav>
+  );
 };
 
 export default Navbar;
