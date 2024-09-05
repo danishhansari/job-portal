36b45cf56d009d7a07d34496eeceab27560f5378
Danish <dan71ish@gmail.com>
2024-09-05 23:51:01 +0530

    fix the unauthorized access

--- src/app/(protected-route)/protected-route.tsx
+++ src/app/(protected-route)/protected-route.tsx
@@ -1,43 +1,23 @@
-// "use client";
+"use client";
 
-// import { useUser } from "@clerk/nextjs";
-// import { ReactNode, useEffect } from "react";
-
-// interface ProtectedRouteProps {
-//   children: ReactNode;
-// }
-
-// const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-//   const { isSignedIn, isLoaded } = useUser();
-
-//   useEffect(() => {
-//     if (isLoaded && !isSignedIn) {
-//       window.location.href = "/?sign-in=true";
-//     }
-//   }, [isLoaded, isSignedIn]);
-
-//   if (!isLoaded || !isSignedIn) {
-//     return null;
-//   }
-
-//   return <>{children}</>;
-// };
-
-// export default ProtectedRoute;
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-// import { useRouter } from "next/navigation";
-import { ReactNode } from "react";
+import { useUser } from "@clerk/nextjs";
+import { ReactNode, useEffect } from "react";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 
-const ProtectedRoute = async ({ children }: ProtectedRouteProps) => {
-  const { userId } = auth();
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { isSignedIn, isLoaded } = useUser();
 
-  if (!userId) {
-    redirect("/?sign-in=true");
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      window.location.href = "/?sign-in=true";
+    }
+  }, [isLoaded, isSignedIn]);
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
   }
 
   return <>{children}</>;
