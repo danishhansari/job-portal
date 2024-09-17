ddd3f3bfe78bcf0988d33e59e47acd3efc9882c6
Danish <dan71ish@gmail.com>
2024-09-17 23:27:20 +0530

    Update footer.tsx

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,9 +1,7 @@
 const Footer = () => {
   return (
     <footer className="p-4 text-center text-slate-400">
-      Made with
-      <span className="text-md mx-1">♥</span>
-      by Danish
+    
     </footer>
   );
 };
